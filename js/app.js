6e76c9ddd63988eba1cc83029942db4b65a1131a
hgalstyan <galstyan.hovhannes@gmail.com>
2017-12-05 20:33:21 +0300

    adding last changes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,29 @@
 const video = document.getElementById('player');
-const span = document.getElementsByTagName('span');
-console.log(video);
+const span = document.getElementById('capture').getElementsByTagName('span');
 
 video.addEventListener('timeupdate', function(){
 	for(let i = 0; i < span.length; i++){
 		let start = span[i].getAttribute('data-startTime');
 		let end = span[i].getAttribute('data-endTime');
 		let videoTime = video.currentTime;
-		if(video >= start && video <=end) {
+		console.log(start, end, videoTime, span[i]);
+		if(videoTime >= start && videoTime <= end) {
 			span[i].classList.add('highlight');
 		}else span[i].classList.remove('highlight');
 	}
 });
+
+for (let i = 0; i < span.length; i++) {
+  span[i].addEventListener('click', function (event) {
+    video.currentTime = event.target.getAttribute("data-startTime");
+    video.play();
+    if( !event ) event = window.event;
+
+  });
+  span[i].addEventListener('mouseover', function () {
+    span[i].classList.add("mouseover");
+  });
+  span[i].addEventListener('mouseout', function () {
+    span[i].classList.remove("mouseover");
+  });
+}
